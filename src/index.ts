845ad86dabc317779cf89623ff74bb6506d231a8
Max Aguilar <zz30.maxzz@gmail.com>
2025-03-12 12:21:14 -0500

    feat(app) adding new hooks to export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as useNiubiz } from "./hooks/useNiubiz";
+export { default as useNiubizApp } from "./hooks/useNiubizApp";
 export { default as useNiubizPay } from "./hooks/useNiubizPay";
+export { default as useNiubizPayApp } from "./hooks/useNiubizPayApp";
