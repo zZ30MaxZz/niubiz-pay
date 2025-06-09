f18b3a613b4e80730575567def063b968af48ffd
Max Aguilar <zz30.maxzz@gmail.com>
2025-06-09 18:32:13 -0500

    feat(forms) adding new form to tokenize only

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export { default as useNiubiz } from "./hooks/useNiubiz";
 export { default as useNiubizApp } from "./hooks/useNiubizApp";
 export { default as useNiubizPay } from "./hooks/useNiubizPay";
 export { default as useNiubizPayApp } from "./hooks/useNiubizPayApp";
+
+export { default as useNiubizToken } from "./hooks/useNiubizToken";
+export { default as useNiubizTokenApp } from "./hooks/useNiubizTokenApp";
+export { default as useNiubizTokenPay } from "./hooks/useNiubizTokenPay";
+export { default as useNiubizTokenPayApp } from "./hooks/useNiubizTokenPayApp";
